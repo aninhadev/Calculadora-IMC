5e514742dc8fd1e6977d9fbbf80b33f876d97402
Ana Maria <anamarianevesdev@gmail.com>
2023-03-28 23:02:51 -0300

    chore: finalization structure javascript of page

--- script.js
+++ script.js
@@ -1,13 +1,49 @@
-function calcular() {
-  let altura = parseFloat(document.getElementById("altura").value);
-  let peso = parseFloat(document.getElementById("peso").value);
-  let imc = peso / (altura * altura);
+function calcularIMC(altura, peso) {
+  const imc = peso / (altura ** 2);
   return imc.toFixed(2);
 }
 
 function exibirResultado() {
-  let resultado = calcular();
-  document.getElementById("res").innerHTML = "Seu IMC é " + resultado;
+  const altura = parseFloat(document.getElementById("altura").value);
+  const peso = parseFloat(document.getElementById("peso").value);
+
+  if (isNaN(altura) || isNaN(peso)) {
+    document.getElementById("res").innerHTML = "Por favor, preencha os campos corretamente";
+    return;
+  }
+
+  const resultado = calcularIMC(altura, peso);
+
+  let mensagem;
+  if (resultado <= 18.5) {
+    mensagem = "Você está em estado de magreza";
+  } else if (resultado <= 24.9) {
+    mensagem = "Você está com um peso saudável";
+  } else if (resultado <= 29.9) {
+    mensagem = "Você está com sobrepeso";
+  } else if (resultado <= 39.9) {
+    mensagem = "Você está com obesidade";
+  } else {
+    mensagem = "Você está com obesidade grave";
+    const minhaImagem = document.createElement('img');
+    minhaImagem.src = 'images/doctorfruits.png';
+    const minhaDiv = document.getElementById('imgdoctor');
+    minhaDiv.appendChild(minhaImagem);
+  }
+
+  document.getElementById("res").innerHTML = `Seu IMC é ${resultado} ${mensagem}`;
 }
 
-document.querySelector('#btnCalcular').addEventListener("click", exibirResultado);
+function limpar() {
+  document.getElementById("peso").value = "";
+  document.getElementById("altura").value = "";
+  document.getElementById("res").innerHTML = "";
+
+  var d = document.getElementById("imgdoctor");
+  var d_interno = document.querySelector('img');
+  var noRemovido = d.removeChild(d_interno);
+
+}
+
+document.getElementById('btnCalcular').addEventListener("click", exibirResultado);
+document.getElementById('btnLimpar').addEventListener("click", limpar);
